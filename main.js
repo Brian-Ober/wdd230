0a1fa0a838616e7fb6b9bccc8bbd4247237ee2c8
Brian-Ober <obe18001@byui.edu>
2022-09-16 10:45:40 -0600

    updated

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 let date = new Date();
 let year = date.getFullYear();
 
-document.querySelector('.year').textContent = year
+document.querySelector('.year').textContent = year;
+
+let updated = document.lastModified;
+document.querySelector('.lastupdated').textContent = updated;
