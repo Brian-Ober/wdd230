d791c582d86a8509bcff67f7f8887d03779c2dbf
Brian-Ober <obe18001@byui.edu>
2022-10-08 21:00:53 -0600

    Added lesson4

---
+++ chamber/js/main.js
@@ -0,0 +1,19 @@
+
+
+const date_now = new Date();
+console.log(date_now)
+const full_date = new Intl.DateTimeFormat('en-US', { dateStyle: 'full'}).format(date_now)
+console.log(full_date)
+document.querySelector('.dateformat').textContent = full_date;
+
+function togglemenu() {
+    document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
+}
+
+let date = new Date();
+let year = date.getFullYear();
+
+document.querySelector('.year').textContent = year;
+
+let updated = document.lastModified;
+document.querySelector('.lastupdated').textContent = updated;
